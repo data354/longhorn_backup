10a119ab0f1f72a34e0128c1d40e85e0073c055a
dykoffi <dykoffi2000@gmail.com>
2023-11-22 06:16:52 +0000

    First commit

---
+++ secrets/minio-secret.sh
@@ -0,0 +1,1 @@
+kubectl create secret generic minio-secret --from-literal=AWS_ACCESS_KEY_ID=k8s --from-literal=AWS_SECRET_ACCESS_KEY=password --from-literal=AWS_ENDPOINTS=https://system.minioapi.eneci.net -n longhorn-system
